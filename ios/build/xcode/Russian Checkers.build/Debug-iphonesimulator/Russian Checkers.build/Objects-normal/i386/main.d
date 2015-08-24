87e39bb8709095927b898a2ad882d1c035d5e201
Egor Churaev <egor.churaev@intel.com>
2015-08-24 09:27:24 +0300

    Added to git.ignore build dir for ios, fixed some problems

--- ios/build/xcode/Russian Checkers.build/Debug-iphonesimulator/Russian Checkers.build/Objects-normal/i386/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/echuraev/Workspace/Projects/RussianCheckers/ios/xcode/Russian\ Checkers/main.cpp \
-  /Applications/Intel/INDE/multi_os_engine/sdk/iphonesimulator/MOE.framework/Headers/MOE.h
